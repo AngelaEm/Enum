33e5b379ae3365056680cbe32e2f339ae40ea189
AngelaEm <angelaemanuelsson@gmail.com>
2023-09-11 10:28:53 +0200

    Added three more rows.

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hej hej!");
             Console.WriteLine("Detta är version 2.");
+            Console.WriteLine("Rad 3.");
+            Console.WriteLine("Rad 4.");
+            Console.WriteLine("Nu lär vi oss GitHub!");
         }
     }
 }
